3d673289907afc65df7f7c0dd63b6e8eacc7484f
tarekbaba <krkr1yemen2@gmail.com>
2025-05-20 00:30:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("تم تشغيل البوت بنجاح!")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("مرحبًا بك في بوت طارق بابا!")
+
+app = ApplicationBuilder().token("8199590505:AAGxPvaO1O8TZHsdsZ5TcYiRkmNnP4s8dME").build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
