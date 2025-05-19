338a47066a0a70f419c74ee5abf46c2d2a3ae6db
tarekbaba <krkr1yemen2@gmail.com>
2025-05-20 02:06:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+import os
+from dotenv import load_dotenv
+load_dotenv("config.env")
+token = os.getenv("TELEGRAM_TOKEN")
+app = ApplicationBuilder().token(token).build()
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("مرحبًا بك في بوت طارق بابا!")
-
-app = ApplicationBuilder().token("8199590505:AAGxPvaO1O8TZHsdsZ5TcYiRkmNnP4s8dME").build()
+await update.message.reply_text("مرحباً بك في بوت طارق بابا!"
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
